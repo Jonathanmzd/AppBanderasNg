46a2a84e868a4622cc8e7c64b1b01d93d8adaee3
Jonathan Ardila <mized2009@gmail.com>
2021-07-07 16:44:44 -0500

    112. RxJs - SwitchMap

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,29 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+
 import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-ver-pais',
   templateUrl: './ver-pais.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class VerPaisComponent implements OnInit {
-
   constructor(
     private activateRoute: ActivatedRoute,
     private paisService: PaisService
-    ) { }
+  ) {}
 
   ngOnInit(): void {
-    this.activateRoute.params
-    .subscribe( ({id}) => {
-      console.log(id);
-      this.paisService.getPaisPorAlpha(id)
-      .subscribe( pais =>{
-        console.log(pais);
-      });
-    });
-  }
+    // this.activateRoute.params
+    // .subscribe( ({id}) => {
+    //   console.log(id);
+    //   this.paisService.getPaisPorAlpha(id)
+    //   .subscribe( pais =>{
+    //     console.log(pais);
+    //   });
+    // });
 
+    // usando un operador de rxjs switchMap toma un observable y retorna otro observable
+    this.activateRoute.params
+      .pipe(switchMap((param) => this.paisService.getPaisPorAlpha(param.id)))
+      .subscribe((resp) => {
+        console.log(resp);
+      });
+  }
 }

1e1e2cfb5c76444b278a8519076e69ce96c66f68
Jonathan Ardila <mized2009@gmail.com>
2021-07-07 13:08:38 -0500

    106. Llenar la tabla de países

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,27 +1,31 @@
 import { Component } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
 import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class PorPaisComponent {
-
   termino: string = '';
   hayError: boolean = false;
+  paises: Country[] = [];
 
-  constructor( private paisService:PaisService) { }
+  constructor(private paisService: PaisService) {}
 
-  buscar(){
+  buscar() {
     this.hayError = false;
     console.log(this.termino);
-    this.paisService.buscarPais(this.termino).subscribe((paises) => {
-      console.log(paises);
-    },(err) => {
-      this.hayError = true;
-    });
+    this.paisService.buscarPais(this.termino).subscribe(
+      (paises) => {
+        console.log(paises);
+        this.paises = paises;
+      },
+      (err) => {
+        this.hayError = true;
+        this.paises = [];
+      }
+    );
   }
-
 }
